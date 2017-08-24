fe720d6b5ce823948c277099b339176e30b47d29
Joao <joaovitornaru@hotmail.com>
2017-08-24 17:52:42 -0300

    Controllers added

---
+++ SizeClassChallenge/ThridViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ThridViewController.swift
+//  SizeClassChallenge
+//
+//  Created by Solid Jaum on 24/08/17.
+//  Copyright © 2017 Jaum. All rights reserved.
+//
+
+import UIKit
+
+class ThridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func exit(segue : UIStoryboardSegue){
+        
+        print("Fui")
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SizeClassChallenge/ViewController.swift
+++ SizeClassChallenge/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +24,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
