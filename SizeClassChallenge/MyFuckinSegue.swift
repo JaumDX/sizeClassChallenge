a7b9062a89f83c64bd1e06ca845269eadca8cc20
Joao <joaovitornaru@hotmail.com>
2017-08-24 22:58:38 -0300

    Data through views and custom segue

---
+++ SizeClassChallenge/MyFuckinSegue.swift
@@ -0,0 +1,13 @@
+//
+//  MyFuckinSegue.swift
+//  SizeClassChallenge
+//
+//  Created by Solid Jaum on 24/08/17.
+//  Copyright © 2017 Jaum. All rights reserved.
+//
+
+import UIKit
+
+class MyFuckinSegue: UIStoryboardSegue {
+
+}
